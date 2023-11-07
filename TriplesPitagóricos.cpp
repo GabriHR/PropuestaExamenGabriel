06a982ab049062839880cca497c5adfe2534935b
GabriHR <ghernrod@myuax.com>
2023-11-07 11:57:06 +0100

    Definicion del valor 'c'.

--- TriplesPitagóricos.cpp
+++ TriplesPitagóricos.cpp
@@ -5,5 +5,9 @@ int main(){
     for (int a=1;a<=17;a++){
         for(int b=a+1;b<=17;b++)
             double c=sqrt(a*a +b*b);
+
+        if(c == (int)c){
+            int c_int=(int)c;
+        }
     }
 }
