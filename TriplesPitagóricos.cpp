a9e7405bd7e4e6913024c5deca3a5ffc191c712f
GabriHR <ghernrod@myuax.com>
2023-11-07 12:02:24 +0100

    Definición del valor 'c' y corrección de un error (6-->'{').

--- TriplesPitagóricos.cpp
+++ TriplesPitagóricos.cpp
@@ -3,11 +3,18 @@
 using namespace std;
 int main(){
     for (int a=1;a<=17;a++){
-        for(int b=a+1;b<=17;b++)
-            double c=sqrt(a*a +b*b);
+        for(int b=a+1;b<=17;b++){
+            double c = sqrt(a*a +b*b);
 
-        if(c == (int)c){
-            int c_int=(int)c;
+            if(c == (int)c) {
+                int c_int=(int)c;
+
+                if(a+b>c_int){
+                    cout<<a<<"-"<<b<<"-"<<c_int<<endl;
+
+                }
+            }
         }
+
     }
 }
